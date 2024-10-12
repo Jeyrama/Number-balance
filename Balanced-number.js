eecd15b7bee593812142399537a165d5b611eaef
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-12 07:13:10 -0700

    Update Balanced-number.js

--- Balanced-number.js
+++ Balanced-number.js
@@ -63,3 +63,12 @@ function balancedNum(number) {
 }
 
 // or
+
+function balancedNum(number){
+  let i, result = 0;
+  number = number + "";
+  for(i = 0; i < number.length / 2 - 1; i++){
+      result += +number[i] - +number[number.length - 1 - i];
+  }
+  return result === 0 ? "Balanced" : "Not Balanced";
+}
